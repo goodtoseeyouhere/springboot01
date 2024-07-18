86c7d04c904c7e7b797d75352f46c71fa8815e24
goodtoseeyouhere <themaninthewall59@gmail.com>
2024-07-18 15:08:45 -0300

    Projeto Criado

---
+++ src/main/java/com/giudicelli/linguisa/LinguisaApplication.java
@@ -0,0 +1,13 @@
+package com.giudicelli.linguisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giudicelli/linguisa/LinguisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giudicelli.linguisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
